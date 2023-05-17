d1121571696296678de389846d179713fc84b33e
JoaquinMarcoss <joaquinmvlr03@gmail.com>
2023-05-17 10:40:48 -0300

    Ejercicios javascript 2da parte terminados

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -15,25 +15,25 @@ let conversorUnidades = (id, valor) => {
         yar = " "; 
         pul = " "; 
         alert("El valor ingresado es incorrecto"); 
-    }else if(id=="metro"){
+    }else if(id==="metro"){
         //conversion de metros
         met = valor; 
         yar = 1.09361*valor;
         pul = 39.3701*valor;
         pie = 3.28084*valor;
-    }else if(id=="pulgada"){
+    }else if(id==="pulgada"){
         //conversion de pulgadas
         pul = valor; 
         yar= 0.0277778*valor;
         met = 0.0254*valor;
         pie = 0.0833333*valor;
-    }else if(id=="pie"){
+    }else if(id==="pie"){
         //conversion de pies
         pie = valor
         yar = 0.333333*valor;
         pul = 12*valor;
         met = 0.304800164592*valor;
-    }else if(id=="yarda"){
+    }else if(id==="yarda"){
         //conversion de yardas
         yar = valor; 
         met = 0.9144*valor;
@@ -53,12 +53,47 @@ let conversorUnidades = (id, valor) => {
  * @param {number} valor - Valor ingresado por el usuario
  */
 
-function convertirGR(id){
-    if(id=="grados"){
-        let gr = document.getElementById("grados").value; 
-        document.getElementById("radianes").value = gr*Math.PI/180; 
+let convertirGR = (id) =>{
+    let gr, rad;
+    if(id==="grados"){
+        gr = document.getElementById("grados").value; 
+        rad = gr*Math.PI/180; 
     }else{
-        let rad = document.getElementById("radianes").value;  
-        document.getElementById("grados").value = rad*180/Math.PI;
+        rad = document.getElementById("radianes").value;  
+        gr = rad*180/Math.PI;
     }
+
+    //asignacion final
+    document.getElementById("radianes").value = rad; 
+    document.getElementById("grados").value = gr; 
+}
+
+/**
+ * Permite visualizar u ocultar un div dentro de la pagina
+ * @method mostrar_ocultar
+ * @param {string} valor - Valor asociado a un button en html
+ */
+
+let mostrar_ocultar = (valor) =>{
+    console.log("El valor enviado a la funcion es: "+valor)
+   if(valor==="val_mostrar"){
+    document.getElementsByName("unDiv")[0].style.display = "block"; 
+   }else{
+    document.getElementsByName("unDiv")[0].style.display = "none";
+   }
+}
+
+
+/**
+ * Suma dos valores ingresados por el usuario
+ * @method sumar
+ */
+
+let sumar = () =>{
+    let num1, num2, res;
+    // tarea que pasa si el usuario ingresa una letra 
+    num1 = document.getElementById("nums1").value;
+    num2 = document.getElementById("nums2").value; 
+    res = Number(num1) + Number(num2); 
+    document.getElementById("totalS").value = res; 
 }
